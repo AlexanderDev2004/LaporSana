c690b548a56ebdd93adbeb30cdd6ab055a902953
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-05-23 17:20:12 +0700

    refactor(auth): remove unused registration routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Route::get('/', function () {
 
 $router->pattern('id', '[0-9]+');
 
-Route::get('register', [AuthController::class, 'register'])->name('register');
-Route::post('register', [AuthController::class, 'postregister'])->name('postregister');
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin'])->name('postlogin');
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
