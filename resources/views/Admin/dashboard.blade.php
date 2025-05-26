eb795b8c5b8dfccdada3bfa7531f685e479630f4
Danennndraaa <danendradhi@gmail.com>
2025-05-26 23:13:58 +0700

    first commit

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin.template')
+
+@section('content')
+
+<div class="card">
+  <div class="card-header">
+    <h3 class="card-title">Halo, ini dashboard LaporSana!</h3>
+    <div class="card-tools"></div>
+  </div>
+  <div class="card-body">
+    Halaman Utama ini lee!!!
+  </div>
+</div>
+@endsection
