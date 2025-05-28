12512639f1ad05a37357153f90af2b2a0763c169
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-05-28 11:44:36 +0700

    feat: Add dataset configuration for dashboard chart and improve logout link structure in sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -23,9 +23,9 @@
                      <p>Dashboard</p>
                  </a>
              </li>
-             <li class="nav-header">
-                 <a href="{{ route('logout') }}" class="nav-icon fas fa-sign-out-alt">
-                     Logout
+             <li class="nav-item">
+                 <a href="{{ route('logout') }}" class="nav-link">
+                     <i class="nav-icon fas fa-sign-out-alt"><p>Logout</p></i>
                  </a>
              </li>
              <li class="nav-header">Manajemen Data Pengguna</li>
