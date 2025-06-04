3b07fbc9d4f7a08b828edb3d550fbcdf740f4c43
Danendra AP <112272169+Danennndraaa@users.noreply.github.com>
2025-06-04 20:32:13 +0700

    remove demo.js

--- resources/views/layouts/sarpras/template.blade.php
+++ resources/views/layouts/sarpras/template.blade.php
@@ -100,9 +100,5 @@
 <script src="{{ asset('LaporSana/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('LaporSana/dist/js/adminlte.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('LaporSana/dist/js/demo.js') }}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('LaporSana/dist/js/pages/dashboard.js') }}"></script>
 </body>
 </html>
